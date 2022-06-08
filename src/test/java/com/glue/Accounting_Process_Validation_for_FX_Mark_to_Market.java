ca9eeb31acc769ad3be3a7d9be3c6953c939b3d5
Charlene <70270938+lenebene@users.noreply.github.com>
2022-06-08 14:07:25 -0500

    Create Accounting_Process_Validation_for_FX_Mark_to_Market.java

---
+++ src/test/java/com/glue/Accounting_Process_Validation_for_FX_Mark_to_Market.java
@@ -0,0 +1,31 @@
+package com.glue;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.junit.Assert;
+
+public class Accounting_Process_Validation_for_FX_Mark_to_Market {
+
+    @Given("^user is on 'File Import Export' page$")
+    public void that_user_is_on_File_Import_Export() throws Throwable {
+        Assert.assertEquals(true, true);
+    }
+
+    @When("^upload the Trade data file from <location> and hit Submit$")
+    public void upload_the_Trade_data_file_from_location_and_hit_Submit() throws Throwable {
+        Assert.assertEquals(true, true);
+    }
+
+    @When("^Run 'Import Accounting Transactions' under schedule process$")
+    public void Run_Import_Accounting_Transactions_under_schedule process() throws Throwable {
+        Assert.assertEquals(true, true);
+    }
+
+    @Then("^Validate Subledger Enteries and View Journal Entry$")
+    public void Validate_Subledger_Enteries_and_View_Journal Entry() throws Throwable {
+        Assert.assertEquals(true, true);
+    }
+    
+    }
+}
